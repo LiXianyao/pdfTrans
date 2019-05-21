978ac8b41f10413aa381286286d9c619ebe9ed2e
buptlixianyao@163.com <18976753003@163.com>
2019-05-21 14:40:53 +0800

    表结构调整，将关系名字-id号抽出到另一张表

--- code/mysql_storage/aux_verification.sql
+++ code/mysql_storage/aux_verification.sql
@@ -1,109 +1,52 @@
--- MySQL dump 10.13  Distrib 5.7.18, for Win64 (x86_64)
---
--- Host: localhost    Database: aux_verification
--- ------------------------------------------------------
--- Server version	5.7.18
+-- we don't know how to generate schema aux_verification (class Schema) :(
+create table if not exists entity_mark
+(
+	id bigint auto_increment
+		primary key,
+	content text not null,
+	passed bit default b'0' not null,
+	reviewed bit default b'0' not null,
+	ver_date date null,
+	user_id bigint null
+)
+charset=utf8
+;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+create table if not exists rela_name_no
+(
+	id bigint auto_increment comment '主键'
+		primary key,
+	rela_name varchar(255) not null comment '关系名称',
+	rela_no varchar(255) not null,
+	constraint rela_name_no_rela_name_uindex
+		unique (rela_name),
+	constraint rela_name_no_rela_no_uindex
+		unique (rela_no)
+)
+;
 
---
--- Table structure for table `entity_mark`
---
+create table if not exists relation_mark
+(
+	id bigint auto_increment
+		primary key,
+	content text not null,
+	passed bit default b'0' not null,
+	relation_id bigint null,
+	reviewed bit default b'0' not null,
+	ver_date date null,
+	user_id bigint null
+)
+charset=utf8
+;
 
-DROP TABLE IF EXISTS `entity_mark`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `entity_mark` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `content` text NOT NULL,
-  `passed` bit(1) NOT NULL DEFAULT b'0',
-  `reviewed` bit(1) NOT NULL DEFAULT b'0',
-  `ver_date` date DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+create table if not exists user
+(
+	id bigint auto_increment
+		primary key,
+	email varchar(64) not null,
+	password varchar(128) not null,
+	phone varchar(11) not null
+)
+charset=utf8
+;
 
---
--- Dumping data for table `entity_mark`
---
-
-LOCK TABLES `entity_mark` WRITE;
-/*!40000 ALTER TABLE `entity_mark` DISABLE KEYS */;
-/*!40000 ALTER TABLE `entity_mark` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `relation_mark`
---
-
-DROP TABLE IF EXISTS `relation_mark`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `relation_mark` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `content` text NOT NULL,
-  `passed` bit(1) NOT NULL DEFAULT b'0',
-  `relation_name` varchar(255) NOT NULL,
-  `relation_no` varchar(255) NOT NULL,
-  `reviewed` bit(1) NOT NULL DEFAULT b'0',
-  `ver_date` date DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `relation_mark`
---
-
-LOCK TABLES `relation_mark` WRITE;
-/*!40000 ALTER TABLE `relation_mark` DISABLE KEYS */;
-/*!40000 ALTER TABLE `relation_mark` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `email` varchar(64) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `phone` varchar(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'liwangadd@gmail.com','202cb962ac59075b964b07152d234b70','');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-05-06 10:32:00
--- code/mysql_storage/relation_mark.py
+++ code/mysql_storage/relation_mark.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import Column,String, Integer, ForeignKey, Date, Text, Boolean
+from sqlalchemy import Column,String, Integer, ForeignKey, Date, Text, Boolean,BigInteger
 from database import Base,db_session
 class RelationMark(Base):
 
@@ -9,31 +9,27 @@ class RelationMark(Base):
     id = Column(Integer,autoincrement=True, primary_key=True)
     content = Column(Text, nullable=False)
     passed = Column(Boolean, default=False)  # 是否审核通过
-    relation_no = Column(String(255), nullable=True)
-    relation_name = Column(String(255), nullable=True)
+    relation_id = Column(BigInteger, nullable=False)
     reviewed = Column(Boolean, default=False)  # 是否被审核过
     ver_date = Column(Date, nullable=True)
     user_id = Column(Integer, nullable=True)  # 是审核数据的人的id！不是数据上传人
     # 查询构造器、、、
     query = db_session.query_property()
 
-    def __init__(self, content, relation_name, relation_no):
+    def __init__(self, content, relation_id):
         self.content = content
-        self.relation_name = relation_name
-        self.relation_no = relation_no
+        self.relation_id = relation_id
 
     def __repr__(self):
         relation_dict = {
-            u"relation_no": self.relation_no,
-            u"relation_name": self.relation_name,
+            u"relation_id": self.relation_id,
             u"content": self.content
         }
         return str(relation_dict)
 
     def __dir__(self):
         relation_dict = {
-            u"relation_no": self.relation_no,
-            u"relation_name": self.relation_name,
+            u"relation_id": self.relation_id,
             u"content": self.content
         }
         return str(relation_dict)
