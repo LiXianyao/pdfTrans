8f818a3d4906f26b2bb49916722df2db0c8dd9e1
buptlixianyao@163.com <18976753003@163.com>
2019-06-06 21:59:17 +0800

    数据表更新：增加statement表，作为entity和relation的关联字段，存储对应句子的原始位置

---
+++ code/mysql_storage/ver_statement.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column,String, Integer, ForeignKey, Date, Text, Boolean
+from database  import Base,db_session
+class VerStatement(Base):
+
+    #表名
+    __tablename__ ='ver_statement'
+    #表结构
+    id = Column(Integer,autoincrement=True, primary_key=True)
+    mark_user = Column(String, nullable=True) # 数据的标记人
+    pdf_no = Column(Integer, nullable=False) # pdf的页码
+    pdf_path = Column(Text, nullable=False)  # pdf的文件名
+    state = Column(Integer, default=-1)  # 3种状态，未审核、审核中、审核完毕
+    user_id = Column(Integer, nullable=True)  # 是审核数据的人的id！不是数据上传人
+    # 查询构造器、、、
+    query = db_session.query_property()
+
+    def __init__(self, pdf_no, pdf_path, mark_user=None):
+        self.pdf_no = pdf_no
+        self.pdf_path = pdf_path
+        if mark_user:
+            self.mark_user = mark_user
+
+    def __repr__(self):
+        statement_dict = {
+            u"pdf_no": self.pdf_no,
+            u"pdf_path": self.pdf_path,
+            u"stat_id": self.id
+        }
+        return str(statement_dict)
+
+    def __dir__(self):
+        statement_dict = {
+            u"pdf_no": self.pdf_no,
+            u"pdf_path": self.pdf_path,
+            u"stat_id": self.id
+        }
+        return str(statement_dict)
+
