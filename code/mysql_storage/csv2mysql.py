a26d09f3994dac4450e02e62c96955f88162de38
buptlixianyao@163.com <18976753003@163.com>
2019-06-07 00:04:03 +0800

    csv结构调整&&数据库更新带来的业务调整

--- code/mysql_storage/csv2mysql.py
+++ code/mysql_storage/csv2mysql.py
@@ -2,6 +2,7 @@
 
 from entity_mark import EntityMark
 from relation_mark import RelationMark
+from ver_statement import VerStatement
 from database import db_session
 import csv
 import sys
@@ -16,15 +17,33 @@ def entity2mysql(time_dir):
     nrows = 0
     entityList = []
     print u"----> 开始存储实体数据..."
+    duplicate_cnt = 0
     for row in data:
         nrows += 1
-        content = "".join([ seg.strip() for seg in row])
-        newEntity = EntityMark(content)
+        pdf_path = row[0].strip()
+        pdf_page = int(row[1].strip())
+        mark_id = int(row[2].strip())
+        statement = db_session.query(VerStatement).filter(
+            VerStatement.pdf_path == pdf_path,
+            VerStatement.pdf_no   == pdf_page,
+            VerStatement.mark_id  == mark_id
+        ).first()
+        if statement != None: ## 一般情况下不该存在同名元组
+            duplicate_cnt += 1
+            continue
+        statement = VerStatement(pdf_no=pdf_page, pdf_path=pdf_path, mark_id = mark_id)
+        db_session.add(statement)
+        db_session.commit()
+
+        stat_id = statement.id
+
+        content = "".join([ seg.strip() for seg in row[3:]])
+        newEntity = EntityMark(content, stat_id)
         entityList.append(newEntity)
     db_session.add_all(entityList)
     db_session.commit()
     del entityList[:]
-    print u"----> 实体数据存储完毕，总共存储数据%d行" % (nrows)
+    print u"----> 实体数据存储完毕，总共存储数据%d行, 冲突id共%d行" % (nrows, duplicate_cnt)
 
 def relation2mysql(time_dir):
     file_path = csv_dir + "relation/%s/relation.csv"%(time_dir)
@@ -36,16 +55,22 @@ def relation2mysql(time_dir):
     cnt_suc = 0
     for row in data:
         nrows += 1
-        try:  # 利用关系编号测试数据行是否有问题
-            content = "".join([ seg.strip() for seg in row[:-2]])
-            relation_no = row[-1].strip()
-            relation_no = int(relation_no)
-            assert (relation_no >0)
-        except:
-            print u"--->>!!第%d行数据发现异常，请检查，异常数据如下: " % (nrows)
-            print "".join(row)
-            continue
-        newRelation = RelationMark(content, relation_no)
+        pdf_path = row[0].strip()
+        pdf_page = int(row[1].strip())
+        mark_id = int(row[2].strip())
+        statement = db_session.query(VerStatement).filter(
+            VerStatement.pdf_path == pdf_path,
+            VerStatement.pdf_no   == pdf_page,
+            VerStatement.mark_id  == mark_id
+        ).first()
+        assert (statement != None) # 必须存在对应的entity
+
+        stat_id = statement.id
+        relation_no = row[4].strip()
+        relation_no = int(relation_no)
+        content = "".join([ seg.strip() for seg in row[5:]])
+
+        newRelation = RelationMark(content, relation_no, stat_id)
         relationList.append(newRelation)
         cnt_suc += 1
     db_session.add_all(relationList)
@@ -55,6 +80,6 @@ def relation2mysql(time_dir):
 
 
 if __name__=="__main__":
-    dir = "20190512"
+    dir = "20190609"
     entity2mysql(dir)
     relation2mysql(dir)
