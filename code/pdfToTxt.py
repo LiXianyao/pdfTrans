d3733015f9efbec2f9a1d80cdb6639d0d4a04a35
LiXianyao <buptlixianyao@163.com>
2019-07-16 17:03:47 +0800

    根据同学的反馈进行调整：1、扩大了页眉的检索范围，对于分段的页眉可以处理的更多了（但还是可能有漏的）；2、对句子分割策略进行调整，当依照分号；或者冒号：分割后，会产生长度小于阈值的短语或者词时，认为不应该以；或：进行分割，此时仅以。！分割；3、处理了做了禁止编辑、复制的加密pdf读取失败的问题。

--- code/pdfToTxt.py
+++ code/pdfToTxt.py
@@ -12,27 +12,21 @@ from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LTTextBoxHorizontal, LAParams
 from pdfminer.pdfpage import PDFTextExtractionNotAllowed, PDFPage
 import re
+import os
 result = []
 class Pdf2TxtManager:
     def __init__(self):
         pass
 
-    def convert_pdf_2_text(self, path):
-        rsrcmgr = PDFResourceManager()
-        retstr = StringIO()
-        device = TextConverter(rsrcmgr, retstr, codec='utf-8', laparams=LAParams())
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        with open('../pdf/' + path, 'rb') as fp:
-            pagecnt = 0
-            for page in PDFPage.get_pages(fp, set()):
-                interpreter.process_page(page)
-                pagecnt += 1
-                if pagecnt == 3:
-                    break
-            text = retstr.getvalue()
-        device.close()
-        retstr.close()
-        return text
+    u"""
+    按照长度规则，判断输入的文本是否可以按照pattern进行分割
+    （场景是为了解决一些句子里的分号；和冒号：分隔开的不是句子，而是短语/词，不应该分开，故加了这个判断）
+    """
+    def judgeSplitable(self, text, pattern):
+        sentences = re.split(pattern, text)
+        llen = map(len, sentences)
+        judgeLen = lambda x: False if x in range(10) else True  # 分裂后的小句长度太短，说明不该分
+        return False not in map(judgeLen, llen[:-1])  # 最后一节可能是句号
 
     def changePdfToTxt(self, fileName):
         pageNum, page_context = self.readPdfInPages(fileName)
@@ -50,52 +44,62 @@ class Pdf2TxtManager:
                     #print pageNo
                     for paragraph in page_context[pageNo]:
                         #print "paragraph=%s" % (paragraph)
-                        sentences = re.split(u"[。；！]", paragraph)
+                        if self.judgeSplitable(paragraph, pattern=u"[。；！]"):
+                            sentences = re.split(u"[。；！]", paragraph)
+                        else:  # 不合适，只按。！划分
+                            sentences = re.split(u"[。！]", paragraph)
                         if len(sentences) == 1 and sentences[0].find(u"：") == -1: # 跳过所有不含。；！：的句子
                             continue
                         #print "has sentence %d" % len(sentences)
                         for s in sentences:
                             s = s.strip()
                             sen = [s]
-                            if s.find(u"：") > 10:
+                            if self.judgeSplitable(s, pattern=u"："):
                                 sen = s.split(u"：")
                             for str in sen:
-                                if len(str) < 5:
+                                if len(str) < 7:
                                     continue
+                                #print "result = %s" % str
                                 f.write(str + "\n")
                     f.write(">>>>>>>>>>>>>>>>>>>>>>>Page %d Finish>>>>>>>>>>>>>>>>>>>>>>>\n" % (pageNo + 1))
                 print "result File %s write finish" % (resFile)
         print "process End"
 
-    def readPdfInPages(self, fileName):
-        file = open('../pdf/' + fileName, 'rb') #二进制读
-        praser = PDFParser(file) #创建pdf文档分析器
-        doc = PDFDocument(praser) #创建一个pdf文档
-        #链接文档与分析器
-        #praser.set_document(doc)
-        #doc.set_parser(praser)
+    def openPdfDoc(self, fileName):
+        file = open(fileName, 'rb')  # 二进制读
+        praser = PDFParser(file)  # 创建pdf文档分析器
+        doc = PDFDocument(praser)  # 创建一个pdf文档
+        return file, praser, doc
 
-        #提供初始化密码,没有就创建一个空的字符串
-        #doc.initialize()
+    def readPdfInPages(self, fileName):
+        formFileName = lambda name: '../pdf/%s' % name
+        try:
+            file, praser, doc = self.openPdfDoc(formFileName(fileName))
+        except:
+            newFileName = formFileName(fileName.replace(".pdf", "_dec.pdf"))
+            os.system('qpdf --password="" --decrypt %s %s' % (formFileName(fileName), newFileName))
+            try:
+                file, praser, doc = self.openPdfDoc(newFileName)  # 创建一个pdf文档
+            except:
+                print(u"有非空密码加密的pdf文件，不可解，转化失败")
+                exit(0)
 
         if not doc.is_extractable: #检测文档是否提供txt转换
-            raise PDFTextExtractionNotAllowed ##使用raise显示地引发异常，后续不再执行
+            raise PDFTextExtractionNotAllowed  # 使用raise显示地引发异常，后续不再执行
         else:
-            #PDF资源管理器
-            pdfRsrcmgr = PDFResourceManager()
-            #创建一个PDF设备对象
-            laparmas = LAParams()
-            device = PDFPageAggregator(pdfRsrcmgr, laparams=laparmas)
-            #PDF解释器对象
-            interpreter = PDFPageInterpreter(pdfRsrcmgr, device)
+            pdfRsrcmgr = PDFResourceManager()  # PDF资源管理器
 
-            pages_context = [] ##准备按页缓存数据
-            #遍历列表，每次处理一个page的内容
+            laparmas = LAParams()
+            device = PDFPageAggregator(pdfRsrcmgr, laparams=laparmas)  # 创建一个PDF设备对象
+            interpreter = PDFPageInterpreter(pdfRsrcmgr, device)  # PDF解释器对象
+
+            pages_context = []  # 准备按页缓存数据
+            #  遍历列表，每次处理一个page的内容
             pagecnt = 0
-            last_page_endl = True ##前一页的内容正常结束
-            last_row_endl = True  ##本页前一行正常结束
+            last_page_endl = True  # 前一页的内容正常结束
+            last_row_endl = True  # 本页前一行正常结束
             page_commen_head = self.get_common_head(doc, interpreter, device)
-            print u"文件公共表头为：%s" % page_commen_head
+            print u"文件公共表头为：%s" % "".join(page_commen_head)
             for page in PDFPage.create_pages(doc):
                 pages_context.append([])  ## a new page
                 interpreter.process_page(page)
@@ -109,16 +113,13 @@ class Pdf2TxtManager:
                 text_layout = []
                 for x in layout:
                     if (isinstance(x, LTTextBoxHorizontal)):
+                        if x.get_text() in page_commen_head:
+                            continue
                         layout_max += 1
                         text_layout.append(x)
-                if len(text_layout):
-                    if text_layout[0].get_text() == page_commen_head:
-                        del text_layout[0]
-                        layout_max -= 1
 
                 for x in text_layout:
                     layout_cnt += 1
-
                     text = x.get_text()
                     if self.has_china_str(text):  # 只处理句子中至少含有一个汉字的数据
                         if text.find(" \n"):  ##包含某段话的结尾
@@ -142,7 +143,7 @@ class Pdf2TxtManager:
                 pagecnt += 1
                 if pagecnt % 100 == 0:
                     print u"已处理%d页数据 " % (pagecnt)
-                #if pagecnt == 10:
+                #if pagecnt == 20:
                 #    break
             print "process end!"
             return pagecnt, pages_context
@@ -170,6 +171,9 @@ class Pdf2TxtManager:
             last_row_endl = endl
         return last_row_endl, last_page_endl
 
+    u"""
+        
+    """
     def lastPage(self, pages_context, pagecnt):
         while pagecnt >= 1:
             pagecnt -= 1
@@ -178,8 +182,10 @@ class Pdf2TxtManager:
         for i in range(len(pages_context)):
             print "Page %d has rows %d" % (i, len(pages_context[i]))
 
-    # 判断是否为汉字字符串
-    # 存在汉字，判断为汉字字符串
+    u"""
+    判断是否为汉字字符串
+    存在汉字，判断为汉字字符串
+    """
     def isChina(self, _str):
         for ch in _str.decode("utf-8"):
             if u'\u4e00' <= ch <= u'\u9fff':
@@ -195,29 +201,37 @@ class Pdf2TxtManager:
 
     # 确定是否有公共文件页眉
     def get_common_head(self, doc, interpreter, device):
-        common_head = dict()
+        common_head_cnt = dict()  # 对多个页首部layout的拼接头进行枚举计数
+        common_head_dict = dict()  # 对多个页首部的layout拼接头的实际进行保存，后续要用来消除页眉
         pagecnt = 0
         for page in PDFPage.create_pages(doc):
             interpreter.process_page(page)
             ##接受该页的LTPage对象
             pagecnt += 1
             layout = device.get_result()
+            layout_buffer = []
             for x in layout:
                 if (isinstance(x, LTTextBoxHorizontal)):  # 获取每页第一个layout
-                    text = x.get_text()
-                    if text not in common_head:
-                        common_head[text] = 0
-                    common_head[text] += 1
-                    break
+                    layout_buffer.append(x.get_text())
+                    text = "".join(layout_buffer)
+                    if text not in common_head_cnt:
+                        common_head_cnt[text] = 0
+                        common_head_dict[text] = layout_buffer[:]
+                    common_head_cnt[text] += 1
+                    if len(layout_buffer) == 10:  # 认为页眉最多分散到5个layout里
+                        break
+            del layout_buffer[:]
             if pagecnt == 30:
                 break
-        common_head = sorted(common_head.items(), key=lambda x: x[1], reverse=True)
-        if common_head[0][1] < 0.8 * pagecnt:
-            return ""
-        return common_head[0][0]
+        common_head_cnt = sorted(common_head_cnt.items(), key=lambda x: (x[1], len(x[0])), reverse=True)
+        common_head, cnt = common_head_cnt[0]
+        #print common_head, cnt
+        if cnt < 0.8 * pagecnt:
+            return []
+        return common_head_dict[common_head]
 
 if __name__=="__main__":
-    file_name = "P020180222582242603860.pdf"
+    file_name = "P020180206577434933482.pdf"
     #file_name = "科大讯飞招股说明书.pdf"
     task = Pdf2TxtManager()
     task.changePdfToTxt(fileName=file_name)
